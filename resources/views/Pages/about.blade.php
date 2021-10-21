081d98c2c41c938492f3e92367a4de9734b13ae6
salim <salimtison@gmail.com>
2021-10-21 14:58:26 +0200

    Config factorisation and includes

--- resources/views/Pages/about.blade.php
+++ resources/views/Pages/about.blade.php
@@ -1,23 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('base')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', ' About | ' . env('APP_NAME'))
 
-    <title>A propos | Hello City</title>
+@section('content')
 
-</head>
 
-<body>
+
     <div>
         Fait avec &hearts; par SuitsConception
         <a href="/">Revenir à la page d'accueil</a>
     </div>
-    <footer>
-        <p>&copy; Copyright {{ date('Y') }} &middot;</p>
-    </footer>
-
-</body>
-
-</html>
+@endsection
--- resources/views/Pages/home.blade.php
+++ resources/views/Pages/home.blade.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('base')
+@section('title')
+    Accueil | {{ env('APP_NAME') }}
+@endsection
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
 
-    <title> Accueil | Hello City</title>
 
-</head>
-
-<body>
     <h1>Hello from France</h1>
     <p>La date et l'heure actuelles sont : {{ strftime('%A %e %B %H:%M') }}</p>
 
-
-    <footer>
-        <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about">A propos de nous</a></p>
-    </footer>
-
-</body>
-
-</html>
+@endsection
